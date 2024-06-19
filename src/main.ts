0ff9526dc3d3c9b6ceb5ef0c1cbc1472c2e26100
zobkazi <zobaidulkazi64.official@gmail.com>
2024-06-19 23:26:36 +0600

    all filre and folder init

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
 import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(morgan('dev'));
   await app.listen(3000);
 }
