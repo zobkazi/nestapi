30fa0ea4e6b77d84791928f0986f4b076b14aa46
zobkazi <zobaidulkazi64.official@gmail.com>
2024-06-19 23:09:52 +0600

    app-module call

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Zobaidul kazi is re1';
+  }
+
+  getProfile(): string {
+    return 'is a progfile of zobaidul kazi';
   }
 }
