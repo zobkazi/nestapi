cf4cafdbbd9f2b12c5929323d94cca528256b250
zobkazi <zobaidulkazi64.official@gmail.com>
2024-06-23 23:31:12 +0600

    login and signup dto added

--- src/modules/auth/dto/authDTO.ts
+++ src/modules/auth/dto/authDTO.ts
@@ -1,8 +1,48 @@
-import { z } from 'zod';
+export class LogInDTO {
+  email: string;
+  password: string;
+  constructor(email: string, password: string) {
+    this.email = email;
+    this.password = password;
+  }
+}
 
-const authSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(6),
-});
+export class SignUpDTO {
+  name: string;
+  email: string;
+  password: string;
+  constructor(name: string, email: string, password: string) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+}
 
-export default authSchema;
+export class LogInResponseDTO {
+  token: string;
+  expiresIn: number;
+  constructor(token: string, expiresIn: number) {
+    this.token = token;
+    this.expiresIn = expiresIn;
+  }
+}
+
+export class SignUpResponseDTO {
+  token: string;
+  expiresIn: number;
+  constructor(token: string, expiresIn: number) {
+    this.token = token;
+    this.expiresIn = expiresIn;
+  }
+}
+
+export class AuthHistoryDTO {
+  email: string;
+  token: string;
+  expiresIn: number;
+  constructor(email: string, token: string, expiresIn: number) {
+    this.email = email;
+    this.token = token;
+    this.expiresIn = expiresIn;
+  }
+}
